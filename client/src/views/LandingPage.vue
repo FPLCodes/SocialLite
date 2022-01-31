700b4c59bd5fe7b2618cfda805f6f07baae529da
Sahab <sahabferdous1@gmail.com>
2022-01-31 09:49:46 +0800

    Added landing page

---
+++ client/src/views/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="BG">
+    Test
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.BG {
+  aspect-ratio: 960/540;
+  width: 100%;
+  height: 60%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-image: url("../assets/blurry-gradient-haikei.svg");
+}
+</style>
